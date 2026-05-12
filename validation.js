47592211aa4f075eab5ee1d85e67c39deeda6ce6
KabirMoulana <your-github-kabirmoulana41@gmail.com>
2026-05-12 15:16:09 +0530

    validation-js

---
+++ validation.js
@@ -0,0 +1,18 @@
+ function validateForm(){
+            let txtName = document.getElementById("firstname").value;
+            let txtEmail = document.getElementById("email").value;
+        
+
+        if(txtName == "")
+        {
+            alert("Please enter the name!");
+            return false;
+        }
+
+        if(txtEmail=="")
+        {
+            alert("Please enter the email!");
+            return false;
+        }
+            return false;
+        }
